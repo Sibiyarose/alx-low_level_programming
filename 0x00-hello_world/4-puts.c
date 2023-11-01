74c4613de3dda67be761638d353ebf423a5be173
Rose <sibiyarose@gmail.com>
2023-11-01 08:24:56 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description; calls puts and receives value returned by puts after its
+ * execution
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
+}
