890b13ddcd196c08182c861bd40f173195aef0d6
Rose <sibiyarose@gmail.com>
2023-11-17 05:42:48 -0800

    script

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of na integer
+ *@n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
