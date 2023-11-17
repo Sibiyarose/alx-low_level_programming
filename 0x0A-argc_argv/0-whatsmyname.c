6202b199f99d9200fe3b1c5a718633b4a77db229
Rose <sibiyarose@gmail.com>
2023-11-17 06:45:07 -0800

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints it own name 
+ * @argc: number of arguments
+ * @argv: arrat with arguments
+ * Return: Alwayas 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
